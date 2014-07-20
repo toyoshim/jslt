d38ff77ed873f863d4331f5a130373b33f1ad81c
Takashi Toyoshima <toyoshim@gmail.com>
2014-07-20 13:24:11 +0000

    update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-node ArrayBuffer_test.js &&
-# DISABLED node chrome.socket_test.js
-node DNS_test.js &&
-node MD5_test.js &&
-node TextModel_test.js &&
-node TextModelConvert_test.js &&
-node Unicode_test.js &&
+run_test () {
+    echo "========================================"
+    echo "Running test $1"
+    echo "----------------------------------------"
+    node $1
+    echo "----------------------------------------"
+    echo "Done."
+    echo "========================================"
+    echo
+}
+
+run_test ArrayBuffer_test.js &&
+# DISABLED run_test chrome.socket_test.js
+run_test DNS_test.js &&
+run_test MD5_test.js &&
+run_test Unicode_test.js &&
+run_test TextModel_test.js &&
+run_test TextModelConvert_test.js &&
+run_test ScreenModel_test.js &&
 exit 0
